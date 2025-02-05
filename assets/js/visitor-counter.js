3b4f1715cfeb23a14bd4142ff53f6f76d5625749
Miroslav Nikolov <miroslav.p.nikolov@gmail.com>
2025-02-05 21:36:25 +0000

    Implement visitors counter

--- assets/js/visitor-counter.js
+++ assets/js/visitor-counter.js
@@ -1,8 +1,13 @@
 
-function setVisitorCounter(){
-    document.getElementById("visitorCounter").textContent  = "Visitor counter: TODO";
+async function fetchAsync () {
+    document.getElementById("visitorCounter").textContent  = "Visitor counter: Loading";
+    let response = await fetch("{{ getenv `HOMEAWS_API_GATEWAY_URL` }}");
+    let data = await response.json();
+    if (data.statusCode == 200) {
+        document.getElementById("visitorCounter").textContent  = "Visitors counter: " + data.body;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    setVisitorCounter();
+    fetchAsync();
 });
