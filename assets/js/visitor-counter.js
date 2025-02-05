eb5c9abffd548810b60e787aab00e9ea2368d2b5
Miroslav Nikolov <miroslav.p.nikolov@gmail.com>
2025-02-05 21:55:43 +0000

    Specify environment in build job

--- assets/js/visitor-counter.js
+++ assets/js/visitor-counter.js
@@ -1,7 +1,7 @@
 
 async function fetchAsync () {
     document.getElementById("visitorCounter").textContent  = "Visitor counter: Loading";
-    let response = await fetch("{{ getenv `HOMEAWS_API_GATEWAY_URL` }}");
+    let response = await fetch("{{ getenv `AWS_API_GATEWAY_URL` }}");
     let data = await response.json();
     if (data.statusCode == 200) {
         document.getElementById("visitorCounter").textContent  = "Visitors counter: " + data.body;
